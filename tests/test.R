a4f75ee90c652b83491809be7e535b2f2afeb95b
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-07-04 10:00:11 +0800

    Update test.R

--- tests/test.R
+++ tests/test.R
@@ -2,11 +2,11 @@ library(testthat, quietly = TRUE)
 suppressPackageStartupMessages(library(tidyverse))
 theme_set(theme_bw())
 
-source("solution_.R") %>%
+source("solution.R") %>%
   suppressMessages() %>%
   suppressPackageStartupMessages()
 test_dir("tests/testthat")
 
 # In GitHub Classroom run this:
-# sudo apt-get update; sudo apt-get install -y r-base-core r-cran-tidyverse r-cran-testthat; sudo Rscript -e 'install.packages("TSP")'
+# sudo apt-get update; sudo apt-get install -y r-base-core r-cran-tidyverse r-cran-testthat
 # Rscript tests/test.R
