c1a86d8089541fe922832a550d994de12fc7262e
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-09-27 13:33:54 +0800

    Update test-1) Check output.R
    Accept `time` as column name too

--- tests/testthat/test-1) Check output.R
+++ tests/testthat/test-1) Check output.R
@@ -1,6 +1,6 @@
 test_that("Dataframe has correct structure and correct number of points", {
   result <- sim_proj(u = 50, theta = 45, h = 10, n = 100)
-  expect_true(all(c("t", "x", "y") %in% names(result)))
+  expect_true(all(c("t", "x", "y") %in% names(result)) | all(c("time", "x", "y") %in% names(result)))
   expect_equal(nrow(result), 100)
   expect_equal(result$y[nrow(result)], 0, tolerance = 1e-2)
 })
